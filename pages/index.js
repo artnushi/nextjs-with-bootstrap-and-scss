7e96763d9c612f9e1be8d7fc4d211f4713e1505d
Art Nushi <art@coda.tech>
2020-12-26 02:23:16 +0100

    Installations and configurations

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Home() {
+    return (
+        <>
+            <div className="container">
+                <h4 className={'mt-5 text-center'}>WELCOME TO NEXT.JS with <span className="text-primary">BOOTSTRAP</span> and SCSS.</h4>
+
+                <h4 className="text-center mt-3">
+                    Created with love from Art Nushi.
+                </h4>
+            </div>
+        </>
+    )
+}
